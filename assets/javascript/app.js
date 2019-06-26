33c4cec02d4c68db6142592561126a0eaccf122a
ab1228 <alexb8734@gmail.com>
2019-06-25 23:45:16 -0500

    finished js file but still needs database and also finshed html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,11 @@
-var trainName = '';
-var destination = '';
-var frequency = '';
-var firstTrain = '';
+// var trainName = '';
+// var destination = '';
+// var frequency = '';
+// var firstTrain = '';
 
-var firebaseConfig = {
-    apiKey: "AIzaSyAIZOZUGsUGmRUM6rq0puYt7CTaUeNdf3s",
-    authDomain: "trainscheduler-fbd30.firebaseapp.com",
-    databaseURL: "https://trainscheduler-fbd30.firebaseio.com",
-    projectId: "trainscheduler-fbd30",
-    storageBucket: "",
-    messagingSenderId: "275128111590",
-    appId: "1:275128111590:web:4ac160f09d14bac8"
-};
+
+// Your web app's Firebase configuration
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 // variable to reference the database
@@ -21,34 +15,41 @@ var database = firebase.database();
 $("#submit").on("click", function (event) {
     event.preventDefault();
     // Grabbed values from text-boxes
-    trainName = $("#train-name-input").val().trim();
-    destination = $("#destination-input").val().trim();
-    frequency = $("#frequency-input").val().trim();
-    firstTrain = $('#first-train-input').val().trim();
+    var trainName = $("#train-name-input").val().trim();
+    var destination = $("#destination-input").val().trim();
+    var frequency = $("#frequency-input").val().trim();
+    var firstTrain = $('#first-train-input').val().trim();
     // "Setting values in the database"
-    newTrain = {
+    var newTrain = {
         trainName: trainName,
         destination: destination,
         frequency: frequency,
         firstTrain: firstTrain
     }
-    // Uploads employee data to the database
+    // // Uploads employee data to the database
     database.ref().push(newTrain);
-
-    // Logs everything to console
-    console.log(newTrain.trainName);
-    console.log(newTrain.destination);
-    console.log(newTrain.frequency);
-    console.log(newTrain.firstTrain);
-
-    alert("New train successfully added");
-
     // Clears all of the text-boxes
     $("#train-name-input").val("");
     $("#destination-input").val("");
     $("#frequency-input").val("");
     $("#first-train-input").val("");
-
 });
 
+database.ref().on('child_added', function (response) {
+    console.log(response.val().destination)
+    console.log(response.val().trainName)
+    console.log(response.val().frequency)
+    console.log(response.val().firstTrain)
+    //ES6 Template Literals
+    //`train Name:  ${response.val().trainName}`
+
+    var tr = `<tr>
+            <td>${response.val().destination}</td>
+            <td>${response.val().trainName}</td>
+            <td>${response.val().frequency}</td>
+            <td>${response.val().trainName}</td>
+        </tr>`
+
+    $("#info-input").append(tr)
+})
 
